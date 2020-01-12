c3d9a1e72d7f69239119f6822ea7f367b01b0d83
Hüseyin Tamer <huseyintamer0@gmail.com>
2020-01-12 21:21:54 +0300

    Between endpoint

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/music', apiRouter);
+app.use('/api/musics', apiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({error:{message: err.message, code:err.code}});
 });
 
 module.exports = app;
--- routes/music.js
+++ routes/music.js
@@ -1,7 +1,70 @@
 const express = require('express');
 const router = express.Router();
 const Music = require('../models/Music');
-router.post('/', (req, res, next) => {
+
+
+router.get('/top10',(req,res)=>{                                    //TOP10
+  const promise = Music.find({}).limit(10).sort({score: -1}); 
+  
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+  res.json(err);
+  })
+  });  
+
+router.get('/',(req,res)=>{                                         //All Songs
+const promise = Music.find({});
+
+promise.then((data)=>{
+  res.json(data);
+}).catch((err)=>{
+res.json(err);
+})
+});
+
+
+router.get('/between/:start_year/:end_year',(req,res)=>{             //Between
+  const {start_year, end_year} = req.params;
+  const promise = Music.find({
+    year: {"$gte": parseInt(start_year), "$lte": parseInt(end_year)}
+  });
+  
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+  res.json(err);
+  })
+  });
+
+router.get('/:music_id',(req, res)=> {                              //Find song
+  const promise = Music.findById(req.params.music_id);
+  promise.then((music)=>{
+    res.json(music);  
+  }).catch(()=> {
+    res.json({message:'The Song is Not Found',code:0});
+  });
+});
+
+router.put('/:music_id',(req, res)=> {                              //Update song
+  const promise = Music.findByIdAndUpdate(req.params.music_id, req.body, {new: true});
+  promise.then((music)=>{
+    res.json(music);  
+  }).catch(()=> {
+    res.json({message:'The Song is Not Found',code:0});
+  });
+});
+
+router.delete('/:music_id',(req, res)=> {                           //Delete song
+  const promise = Music.findByIdAndRemove(req.params.music_id);
+  promise.then((music)=>{
+    res.json({music,message:'The Song has been is Deleted',code:1});  
+  }).catch(()=> {
+    res.json({message:'The Song is Deleted',code:1});
+  });
+});
+
+router.post('/', (req, res) => {                                     //Add song
   const music = new Music(req.body);
  const promise = music.save();
 
