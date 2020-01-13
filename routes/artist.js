45875fc8b866abc6ff1743de22ca89e481ecfcf0
Hüseyin Tamer <huseyintamer0@gmail.com>
2020-01-13 20:35:03 +0300

    Find Group by ID

--- routes/artist.js
+++ routes/artist.js
@@ -1,8 +1,9 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Artist = require('../models/Artist');
 
-router.get(('/'),(req, res, next)=>{
+router.get(('/'),(req, res, next)=>{    //Combine Songs
 const promise = Artist.aggregate([
     {
         $lookup: {
@@ -49,6 +50,59 @@ promise.then((data)=>{
     res.json(err);
 })
 });
+
+router.get(('/:group_id'),(req, res, next)=>{            //Find by ID
+    const promise = Artist.aggregate([
+        {
+            $match: {
+                '_id':mongoose.Types.ObjectId(req.params.group_id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'musics',
+                localField: '_id',
+                foreignField: 'group_id',
+                as: 'musics'
+    
+    
+            }
+        },
+        {
+            $unwind:{
+                path: '$musics',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio',
+                },
+                musics:{
+                    $push: '$musics'
+                }
+            }
+        },
+        {
+            $project:{
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                musics: '$musics'
+            }
+        }
+    
+    ])
+    promise.then((data)=>{
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+    });
 
 router.post('/',(req,res,next)=>{                   //Artist ADD
     const artist = new Artist(req.body);
