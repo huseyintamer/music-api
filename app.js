8f171e89c7365c3e9b2fcebb4434657349c4f6d9
Hüseyin Tamer <huseyintamer0@gmail.com>
2020-01-20 00:55:07 +0300

    Mocha ilk test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', verifyToken);
+app.use('/api/', verifyToken);
 app.use('/api/musics', apiRouter);
 app.use('/api/artists', artistRouter);
 
